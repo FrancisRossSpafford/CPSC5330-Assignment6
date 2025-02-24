abb1441c8b342d3b137977fa0923b72c2ae8ccc3
Francis Ross Spafford <spafffr@auburn.edu>
2025-02-23 21:47:03 -0600

    Fixed issue with orpaned link.

--- Assignment6/Model/CurrencyLogic.swift
+++ Assignment6/Model/CurrencyLogic.swift
@@ -5,3 +5,52 @@
 //  Created by Ross Spafford on 2/23/25.
 //
 
+import Foundation
+
+struct CurrencyLogic {
+    
+    var roundedAmount : Int = 0
+    
+    var showGBP: Bool = false
+    var showCDN: Bool = false
+    var showEUR: Bool = false
+    var showPeso: Bool = false
+    
+   
+    
+    mutating func setShowGBP(_ switchValue: Bool) {
+        if switchValue {
+            showGBP = true
+        } else {
+            showGBP = false
+        }
+    }
+    
+    mutating func setShowCDN(_ switchValue: Bool) {
+        if switchValue {
+            showCDN = true
+        } else {
+            showCDN = false
+        }
+    }
+    
+    mutating func setShowEUR(_ switchValue: Bool) {
+        if switchValue {
+            showEUR = true
+        } else {
+            showEUR = false
+        }
+    }
+    
+    mutating func setShowPeso(_ switchValue: Bool) {
+        if switchValue {
+            showPeso = true
+        } else {
+            showPeso = false
+        }
+    }
+    
+    
+    
+    
+}
--- Assignment6/View/ViewController.swift
+++ Assignment6/View/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    
+    @IBOutlet weak var Amount: UITextField!
+    
+    var currencyLogic = CurrencyLogic()
+    var amount : String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func GBPChanged(_ sender: UISwitch) {
+        currencyLogic.setShowGBP(sender.isOn)
+    }
+    
+    @IBAction func CDNChanged(_ sender: UISwitch) {
+        currencyLogic.setShowCDN(sender.isOn)
+    }
+    
+    @IBAction func EURChanged(_ sender: UISwitch) {
+        currencyLogic.setShowEUR(sender.isOn)
+    }
+    
+    @IBAction func PesoChanged(_ sender: UISwitch) {
+        currencyLogic.setShowPeso(sender.isOn)
+    }
+    
+    @IBAction func Convert(_ sender: UIButton) {
+        //amount = loanLogic.calculateLoanAmount()
+        self.performSegue(withIdentifier: "MainTransition", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "MainTransition" {
+            let loanNavigation = segue.destination as! ConversionViewController
+            //loanNavigation.amount = amount
+        }
+    }
 }
 
